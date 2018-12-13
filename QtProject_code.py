b75039e291f0cc8dadb15f140f9b0a885b356212
Moromug <moromug@yandex.ru>
2018-12-13 18:55:11 +0300

    Made code template

---
+++ QtProject_code.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QWidget, QApplication, QLineEdit, QLabel, QPushButton, QMainWindow
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('QtProject_design.ui', self)
+        self.btn1.clicked.connect(self.Algerba)
+        self.btn2.clicked.connect(self.Trigonometry)
+
+    def Algebra(self):
+        pass
+
+    def Trigonometry(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
